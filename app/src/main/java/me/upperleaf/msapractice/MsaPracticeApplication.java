578ed61e28f18cb76fbcf9e8ba2ae0ef10595c8c
UpperLeaf <tkdduq8260@gmail.com>
2020-09-24 15:24:00 +0900

    Make Deafult Project Structure

---
+++ app/src/main/java/me/upperleaf/msapractice/MsaPracticeApplication.java
@@ -0,0 +1,13 @@
+package me.upperleaf.msapractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsaPracticeApplication.class, args);
+    }
+
+}
---
+++ app/src/test/java/me/upperleaf/msapractice/MsaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.upperleaf.msapractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
